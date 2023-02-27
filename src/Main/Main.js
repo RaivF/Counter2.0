81fa8370efdc51a3aaebe3a2dab12890d21f6c92
RaivF <122525726+RaivF@users.noreply.github.com>
2023-02-27 16:39:32 +0400

    first commit

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,24 +1,37 @@
 import React from "react";
 import s from './Main.module.css'
+import './main.css'
 
 let Main = () => {
 
     const [count, setCount] = React.useState(0);
+    let maxCount = 5
     function counter() {
-        setCount(count+1)
+        if (count == maxCount-1){
+            document.getElementById("1").classList.add("resultSpanRed")
+        }
+        if (count == maxCount) {
+            setCount(count)
+        }else{
+        setCount(count + 1)}
     }
-    function  reserCounter(){
+    function reserCounter() {
         setCount(count - count)
+        document.getElementById("1").classList.remove("resultSpanRed")
     }
     return (
-
         <div className={s.mainContainer}>
             <div className={s.container}>
-                <div className={s.resulrCounterPlase}><span>{count}</span></div>
+                <div className={s.resulrCounterPlase}><span id="1" className="resultSpan">{count}</span></div>
                 <div className={s.containerButton}>
-                    <button onClick={()=>{counter()
-                        console.log(counter)}}>inc</button>
-                    <button onClick={()=>{reserCounter()}}>reset</button>
+                    <button onClick={() => {
+                        counter()
+                    }}>inc
+                    </button>
+                    <button onClick={() => {
+                        reserCounter()
+                    }}>reset
+                    </button>
                 </div>
             </div>
         </div>
